7f1668d785c10b10f45107485866344972aeeb1e
Besufekad-HAZ <besurye@gmail.com>
2023-10-24 00:39:04 +0300

    Setup mongodb connection and create schema and models

---
+++ models/Note.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema(
+    {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+},
+
+{
+    timestamps: true
+}
+)
+
+module.exports = mongoose.model('Note', noteSchema);
