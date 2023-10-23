e07a2a6eb215510f9701fc4925b392cbda06d96b
Besufekad-HAZ <besurye@gmail.com>
2023-10-23 22:45:41 +0300

    Setup the backend of the project and add PORTS for the Rest-API

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+
+app.use('/', require('./routes/root'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
