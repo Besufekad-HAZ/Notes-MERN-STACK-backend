dee6fde2a8fcc48179dcd84c2f577026b3848d89
Besufekad-HAZ <besurye@gmail.com>
2023-10-24 00:49:55 +0300

    Setup mongoose databse connection with connection string

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500
 
+
 console.log(process.env.NODE_ENV);
+
+
+
 
 app.use(logger)
 
