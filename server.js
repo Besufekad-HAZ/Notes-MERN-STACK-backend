1c52e0066af914ab16aca581d0dc0714b320d4b4
Besufekad-HAZ <besurye@gmail.com>
2023-10-24 00:56:23 +0300

    Connected to the athlas cluster and finished creating Models

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
+const { connect } = require('http2')
 const PORT = process.env.PORT || 3500
 
 
 console.log(process.env.NODE_ENV);
 
-
-
+connectDB();
 
 app.use(logger)
 
@@ -42,4 +42,18 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+
+mongoose.connection.on('error', err => {
+    console.error(err)
+    logEvents(
+        `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+        'mongoErrLog.log'
+    )
+    process.exit(1)
+})
