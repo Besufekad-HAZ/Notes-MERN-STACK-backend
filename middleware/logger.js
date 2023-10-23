ec998205b166b52562d1b4611e3e207f6df16b2c
Besufekad-HAZ <besurye@gmail.com>
2023-10-23 23:46:20 +0300

    Use CORS and add allowed Origins options for credentials and access rights

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,8 @@
-const {format} = require('util');
-const {v4: uuid} = require('uuid');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-const path = require('path');
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
 
 const logEvents = async (message, logFileName) => {
     const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const { logger } = require('./middleware/logger');
-const { errorHandler } = require('./middleware/errorHandler');
-const PORT = process.env.PORT || 3500;
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const PORT = process.env.PORT || 3500
 
+app.use(logger)
 
-app.use(logger);
+app.use(cors(corsOptions))
 
-app.use(express.json());
+app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
 
-app.use('/', require('./routes/root'));
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
     res.status(404)
@@ -25,6 +31,6 @@ app.all('*', (req, res) => {
     }
 })
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
