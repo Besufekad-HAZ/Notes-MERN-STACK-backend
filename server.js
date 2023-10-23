659666b7b6d4f1fe20581cf8aba96a3c0ff173bd
Besufekad-HAZ <besurye@gmail.com>
2023-10-23 22:56:52 +0300

    Handle 404 bad request error in the routes

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 3500;
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+})
 
 app.use('/', require('./routes/root'));
 
