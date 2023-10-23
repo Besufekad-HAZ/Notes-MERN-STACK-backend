e821eb11e64491a86f76426f44cbe76a447037fa
Besufekad-HAZ <besurye@gmail.com>
2023-10-24 00:44:20 +0300

    Use mongoose sequence to generate user ticket numbers

--- models/Note.js
+++ models/Note.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const noteSchema = new mongoose.Schema(
     {
@@ -12,5 +13,11 @@ const noteSchema = new mongoose.Schema(
     timestamps: true
 }
 )
+
+noteSchema.plugin(AutoIncrement, {
+    inc_field: 'ticket',
+    id: 'ticketNums',
+    start_seq: 500,
+});
 
 module.exports = mongoose.model('Note', noteSchema);
