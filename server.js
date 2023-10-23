c057cd92f6e99300cf0716cd7de44c3fd966950c
Besufekad-HAZ <besurye@gmail.com>
2023-10-23 23:28:02 +0300

    Work with error handlers and loggers

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const { errorHandler } = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+
+
+app.use(logger);
 
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+app.use('/', require('./routes/root'));
+
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({ error: '404 Not Found' });
+        res.json({ message: '404 Not Found' })
     } else {
-        res.type('txt').send('404 Not Found');
+        res.type('txt').send('404 Not Found')
     }
 })
 
-app.use('/', require('./routes/root'));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
