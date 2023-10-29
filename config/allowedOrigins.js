f4efd1e9241dbad41ed9eaf768d69bf1eedbdd8c
Besufekad-HAZ <besurye@gmail.com>
2023-10-29 13:26:10 +0300

    Minor fixes for deployement

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
-    'https://bemni-tech-repair.onrender.com/',
+    'https://bemni-tech-repair.onrender.com',
     'chrome-extension://gmmkjpcadciiokjpikmkkmapphbmdjok'
 ]
 
